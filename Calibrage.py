06bd6d8502c2b20fa72be1242b5c6107738c602c
Ant89ne <antoine.bralet@cpe.fr>
2020-10-01 09:07:16 +0200

    Save option OK

--- Calibrage.py
+++ Calibrage.py
@@ -7,6 +7,8 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 cap = cv2.VideoCapture(0)
+k = 0
+
 while(True):
     ret, frame = cap.read() #1 frame acquise à chaque iteration
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #conversion en n&b
@@ -14,6 +16,11 @@ while(True):
     key = cv2.waitKey(1) #on évalue la touche pressée
     if key & 0xFF == ord('q'): #si appui sur'q'
         break #sortie de la boucle while
+    if key == ord('c'):
+        #enregistre image
+        cv2.imwrite("Capture" + str(k)+".png", gray)
+        k+=1
+
 cap.release()
 cv2.destroyAllWindows()
 
