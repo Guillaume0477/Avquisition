fbb92aa9b1bd48c7d5e9aed5e58c7187f43a96a4
Guillaume0477 <guillaumeduret1@gmail.com>
2020-10-01 10:51:25 +0200

    ad tf affine

--- Calibrage.py
+++ Calibrage.py
@@ -10,6 +10,21 @@ from matplotlib import pyplot as plt
 cap = cv2.VideoCapture(0)
 k = 0
 
+ret, frame = cap.read()
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+rows,cols = gray.shape
+
+tx=15
+ty=10
+angle=180
+echelle=0.5
+
+Mr = cv2.getRotationMatrix2D((cols/2,rows/2),angle,echelle)
+
+Mt = np.float32([[0,0,tx],[0,0,ty]])
+
+M = Mr + Mt
+
 while(True):
     ret, frame = cap.read() #1 frame acquise à chaque iteration
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #conversion en n&b
@@ -17,8 +32,11 @@ while(True):
 
     edges = cv2.Canny(gray,100,200)
 
+    dst = cv2.warpAffine(gray,M,(cols,rows))
+
     cv2.imshow('Capture_Video', gray) #affichage
     cv2.imshow('Capture_Contours', edges) #affichage
+    cv2.imshow('Capture_Affine', dst) #affichage
 
     key = cv2.waitKey(1) #on évalue la touche pressée
 
